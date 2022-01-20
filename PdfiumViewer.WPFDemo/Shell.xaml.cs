0b3f637c2400c76d3908a53678eb28f92c41082e
anyu2021 <anyu2021@unipus.cn>
2022-01-20 14:24:30 +0800

    update：1、升级pdfium依赖 2、升级demo framework为4.8

---
+++ PdfiumViewer.WPFDemo/Shell.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System.Windows;
+
+namespace PdfiumViewer.WPFDemo
+{
+    /// <summary>
+    /// Shell.xaml 的交互逻辑
+    /// </summary>
+    public partial class Shell : Window
+    {
+        PdfDocument pdfDoc;
+
+        public Shell()
+        {
+            InitializeComponent();
+        }
+
+        private void LoadPDFButton_Click(object sender, RoutedEventArgs e)
+        {
+            using (var dialog = new System.Windows.Forms.OpenFileDialog())
+            {
+                dialog.Filter = "PDF Files (*.pdf)|*.pdf|All Files (*.*)|*.*";
+                dialog.Title = "Open PDF File";
+
+                if (dialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+                {
+                    pdfDoc = PdfDocument.Load(dialog.FileName);
+                    pdfviewer.Document = pdfDoc;
+                }
+            }
+        }
+    }
+}
